7bba45b50d61e20b449eab4e774c0ad1f6e65215
Angoosh <kaplanantonin@gmail.com>
2019-09-24 15:35:30 +0200

    search enabled editing

--- Cajka_Inventar.py
+++ Cajka_Inventar.py
@@ -7,7 +7,6 @@ def Open(file):
     line = []
     with open(file+".csv") as file:
         csv_reader = csv.reader(file, delimiter=',')
-        #line_count = 0
         for row in csv_reader:
             line.append(row)
     
@@ -17,7 +16,6 @@ def List(file):
     line = []
     with open(file+".csv") as file:
         csv_reader = csv.reader(file, delimiter=',')
-        #line_count = 0
         for row in csv_reader:
             line.append(row)
 
@@ -30,12 +28,12 @@ def Edit():
         if i == 0:
             pass
         else:
-            print(line[i][0]+": "+line[i][1])
+            print(line[i][1]+": "+line[i][2])
             x = input("New value: ")
             if x == "":
                 pass
             else:
-                line[i][1] = x
+                line[i][2] = x
 
     for i in range(len(line)):
         print(line[i])
@@ -54,32 +52,52 @@ def Append(file):
         item = input("Polozka: ")
         qty = input("Mnozstvi: ")
         price = input("Cena: ")
+        ID = "ID"+str(len(line)-1)
         
-        add = [item, qty, price]
+        add = [ID, item, qty, price]
         
         fileWrite.writerow(add)
+        
+def Search():
+    search = "ID"+input("Item: ")
+    for i in range(len(line)):
+        try:
+            line[i].index(search)
+            print(line[i])
+            return i
+        except:
+            pass
+ 
+def Edit_Adv():
+    s = Search()
+    print(line[s][1]+": "+line[s][2])
+    x = input("New value: ")
+    if x == "":
+        pass
+    else:
+        line[s][2] = x
+    
        
 def Default(file): 
     with open(file+".csv", "w", newline='') as file:
         fileWrite = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         
-        fileWrite.writerow(["Polozka","Pocet","Cena"])
-        fileWrite.writerow(["Masala_Chai","250g",268])
-        fileWrite.writerow(["Vanilla","100g",168])
+        fileWrite.writerow(["ID", "Polozka","Mnozstvi","Cena"])
+        fileWrite.writerow(["ID0", "Masala_Chai",250 ,268])
+        fileWrite.writerow(["ID1", "Vanilla",100 ,168])
         
 
 if __name__ == "__main__":
-    """
-    Default(FILE)
-    """
+
     while True:
-        a = input("Edit [E], Append [A], List[L] or Exit [Exit]? ")
+        a = input("Edit [E], Append [A], List[L], Search[S] or Exit [Exit]? ")
         if a == "E" or a == "e":
             line = Open(FILE)
-            Edit()
+            Edit_Adv()
             Write(FILE)
         elif a == "A" or a == "a":
             while True:
+                line = Open(FILE)
                 Append(FILE)
                 a = input("Next item? [Y/n] ")
                 if a == "Y" or a == "y":
@@ -90,6 +108,18 @@ if __name__ == "__main__":
             line = List(FILE)
         elif a == "Exit" or a == "exit":
             break
+        elif a == "S" or a == "s":
+            line = Open(FILE)
+            Search()
+        elif a == "B" or a == "b":
+            line = Open(FILE)
+            Edit()
+            Write(FILE)
+        elif a == "Default":
+            x = input("Really? [Y/n] ")
+            if x == "Y" or x == "y":
+                Default(FILE)
+            else:
+                pass
         else:
             print("Invalid option!\n")
-    
